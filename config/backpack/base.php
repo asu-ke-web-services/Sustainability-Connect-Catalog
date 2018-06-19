51e93cea86b66fe467e9d553c3754a2ff230881d
Nathan Rollins <nathan.rollins@asu.edu>
2018-06-19 10:12:11 -0700

    Update config

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -12,20 +12,20 @@ return [
     */
 
     // Project name. Shown in the breadcrumbs and a few other places.
-    'project_name' => 'Backpack',
+    'project_name' => 'SC Catalog',
 
     // Menu logos
-    'logo_lg'   => '<b>Back</b>pack',
-    'logo_mini' => '<b>B</b>p',
+    'logo_lg'   => '<b>Sus</b>Catalog',
+    'logo_mini' => '<b>S</b>C',
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
+    'developer_name' => 'Julie Ann Wrigley Global Institute of Sustainability',
 
     // Developer website. Link in footer.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => 'https://sustainability.asu.edu',
 
     // Show powered by Laravel Backpack in the footer?
-    'show_powered_by' => true,
+    'show_powered_by' => false,
 
     // The AdminLTE skin. Affects menu color and primary/secondary colors used throughout the application.
     'skin' => 'skin-purple',

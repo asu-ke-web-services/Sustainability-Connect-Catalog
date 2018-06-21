fff9d2ee6b5c0e51018d12eaa7796109ad96fe5f
Nathan Rollins <nathan.rollins@asu.edu>
2018-06-20 17:21:16 -0700

    Generate scaffolding for Organization

---
+++ app/Repositories/OrganizationRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Organization;
+use InfyOm\Generator\Common\BaseRepository;
+
+/**
+ * Class OrganizationRepository
+ * @package App\Repositories
+ * @version June 20, 2018, 11:29 pm UTC
+ *
+ * @method Organization findWithoutFail($id, $columns = ['*'])
+ * @method Organization find($id, $columns = ['*'])
+ * @method Organization first($columns = ['*'])
+*/
+class OrganizationRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'organization_type_id',
+        'organization_status_id',
+        'name'
+    ];
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Organization::class;
+    }
+}

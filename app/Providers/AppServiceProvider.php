dbbda7ed67878f9e76d58e129cab34fd425ae626
Nathan Rollins <nathan.rollins@asu.edu>
2018-06-29 10:25:33 -0700

    Bind repo contracts to repo implementations for DI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(\SCCatalog\Contracts\Repository\AddressRepositoryContract::class, \SCCatalog\Repositories\AddressRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\CategoryRepositoryContract::class, \SCCatalog\Repositories\CategoryRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\InternshipRepositoryContract::class, \SCCatalog\Repositories\InternshipRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\KeywordRepositoryContract::class, \SCCatalog\Repositories\KeywordRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\OpportunityRepositoryContract::class, \SCCatalog\Repositories\OpportunityRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\OpportunityStatusRepositoryContract::class, \SCCatalog\Repositories\OpportunityStatusRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\OpportunityTypeRepositoryContract::class, \SCCatalog\Repositories\OpportunityTypeRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\OrganizationStatusRepositoryContract::class, \SCCatalog\Repositories\OrganizationStatusRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\OrganizationTypeRepositoryContract::class, \SCCatalog\Repositories\OrganizationTypeRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\ProjectRepositoryContract::class, \SCCatalog\Repositories\ProjectRepository::class);
+        $this->app->bind(\SCCatalog\Contracts\Repository\StudentDegreeLevelRepositoryContract::class, \SCCatalog\Repositories\StudentDegreeLevelRepository::class);
     }
 }

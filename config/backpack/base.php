34c2c6110c2ce212de55ca101f1e65fbb1050539
Nathan Rollins <nathan.rollins@asu.edu>
2018-07-02 13:51:35 -0700

    Fix namespace ref for BackpackUser model

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -79,7 +79,7 @@ return [
     */
 
     // Fully qualified namespace of the User model
-    'user_model_fqn' => App\Models\BackpackUser::class,
+    'user_model_fqn' => SCCatalog\Models\BackpackUser::class,
 
     // The classes for the middleware to check if the visitor is an admin
     // Can be a single class or an array of clases

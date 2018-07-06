f372aec9d9c55aef9ed642036e4bfa1d4579e20c
Nathan Rollins <nathan.rollins@asu.edu>
2018-07-06 13:11:51 -0700

    Stage preliminary code for Blade partials (components)
    Not usable right now

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,92 @@
+		<meta charset="UTF-8">
+		<meta content="width=device-width, initial-scale=1" name="viewport">
+		<meta content="IE=edge" http-equiv="X-UA-Compatible">
+		<title>{{ $title }} - Sustainability Connect</title>
+		<link href="http://gmpg.org/xfn/11" rel="profile">
+
+		<link href="https://sustainabilityconnect.asu.edu/" rel="canonical">
+		<meta content="en_US" property="og:locale">
+		<meta content="website" property="og:type">
+		<meta content="Home - Sustainability Connect" property="og:title">
+		<meta content="https://sustainabilityconnect.asu.edu/" property="og:url">
+		<meta content="Sustainability Connect" property="og:site_name">
+		<meta content="https://sustainabilityconnect.asu.edu/files/2016/08/GIOS_ASU_Logo.png" property="og:image">
+		<meta content="summary" name="twitter:card">
+		<meta content="Home - Sustainability Connect" name="twitter:title">
+		<meta content="@ASUSustConnect" name="twitter:site">
+		<meta content="https://sustainabilityconnect.asu.edu/files/2016/08/GIOS_ASU_Logo.png" name="twitter:image">
+		<meta content="@ASUSustConnect" name="twitter:creator">
+
+		<link href="https://sustainabilityconnect.asu.edu/feed/" title="Sustainability Connect » Feed" type="application/rss+xml" rel="alternate">
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/css/bootstrap-asu.min.css">
+        <link rel="stylesheet" href="https://static.sustainability.asu.edu/asu-theme/asu-header/css/asu-nav.css">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+        @stack('styles')
+
+		<script type="text/javascript" async="" src="https://www.google-analytics.com/plugins/ua/linkid.js"></script>
+		<script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+		<script async="" src="//www.googletagmanager.com/gtm.js?id=GTM-KDWN8Z&amp;l=SI_dataLayer"></script>
+		<script async="" src="//www.google-analytics.com/analytics.js"></script>
+		<script type="application/ld+json">{"@context":"http:\/\/schema.org","@type":"WebSite","url":"https:\/\/sustainabilityconnect.asu.edu\/","name":"Sustainability Connect","potentialAction":{"@type":"SearchAction","target":"https:\/\/sustainabilityconnect.asu.edu\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script>
+		<style type="text/css">
+			img.wp-smiley,
+			img.emoji {
+				display: inline !important;
+				border: none !important;
+				box-shadow: none !important;
+				height: 1em !important;
+				width: 1em !important;
+				margin: 0 .07em !important;
+				vertical-align: -0.1em !important;
+				background: none !important;
+				padding: 0 !important;
+			}
+		</style>
+		<script type="text/javascript">
+            if ( top.frames.length != 0 ) {
+                if ( window.location.href.replace )
+                    top.location.replace( self.location.href );
+                else
+                    top.location.href = self.document.href;
+            }
+        </script>
+        <script
+            type="text/javascript"
+            src="https://code.jquery.com/jquery-2.2.4.min.js"
+            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+            crossorigin="anonymous"></script>
+        <script type="text/javascript" src="{{ asset('vendor/tether/js/tether.js') }}"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous" defer></script>
+        <script type="text/javascript" src="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/js/bootstrap-asu.min.js" defer></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/patternfly/3.24.0/js/patternfly.min.js" defer></script>
+        <script type="text/javascript" src="https://static.sustainability.asu.edu/asu-theme/asu-header/js/asu-header.min.js" defer></script>
+        <script type="text/javascript" src="https://static.sustainability.asu.edu/asu-theme/asu-header/js/asu-header-config.js" defer></script>
+        <script type="text/javascript" defer>$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip({'placement': 'top'});});</script>
+        @stack('scripts')
+
+
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/apple-touch-icon-60x60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/apple-touch-icon-152x152.png" />
+        <link rel="icon" type="icon" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/favicon.ico" />
+        <link rel="icon" type="image/png" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/favicon-196x196.png" sizes="196x196" />
+        <link rel="icon" type="image/png" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/favicon-128.png" sizes="128x128" />
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-TileImage" content="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/mstile-70x70.png" />
+        <meta name="msapplication-square150x150logo" content="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/mstile-150x150.png" />
+        <meta name="msapplication-wide310x150logo" content="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/mstile-310x150.png" />
+        <meta name="msapplication-square310x310logo" content="https://static.sustainability.asu.edu/asu-theme/asu-web-standards/img/favicon/mstile-310x310.png" />

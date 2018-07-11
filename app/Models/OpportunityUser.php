6fa3917ffd903f72b738e9010facc5b356a4df60
Nathan Rollins <nathan.rollins@asu.edu>
2018-07-11 14:30:42 -0700

    Generate OpportunityUser pivot model

---
+++ app/Models/OpportunityUser.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SCCatalog;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class OpportunityUser extends Pivot
+{
+    //
+}
---
+++ database/migrations/2018_07_11_204950_create_opportunities_users_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpportunitiesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opportunities_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('opportunity_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('relationship_type_id')->unsigned();
+            $table->boolean('primary');
+            $table->text('comments')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->integer('deleted_by')->unsigned()->nullable();
+
+            $table->foreign('relationship_type_id')
+                ->references('id')->on('relationship_types');
+
+            $table->foreign('opportunity_id')
+                ->references('id')->on('opportunities');
+
+            $table->foreign('user_id')
+                ->references('id')->on('users');
+
+            $table->foreign('created_by')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('updated_by')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('deleted_by')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opportunities_users');
+    }
+}

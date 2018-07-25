add887f7635485e7e683e2142015539d864d4df9
Nathan Rollins <nathan.rollins@asu.edu>
2018-07-24 17:26:39 -0700

    Add frontend password auth for entire site while on preview server

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            \ElicDev\SiteProtection\Http\Middleware\SiteProtection::class,
             \SCCatalog\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,

ad83cfa9b267a8cded6944096b03d1e689e8f289
Nathan Rollins <nathan.rollins@asu.edu>
2018-07-25 19:35:09 -0700

    Remove redundant header in project idea form

--- resources/views/projects/create_idea.blade.php
+++ resources/views/projects/create_idea.blade.php
@@ -10,11 +10,6 @@
 @endpush
 
 @section('content')
-    <section class="content-header">
-        <h1>
-            Project
-        </h1>
-    </section>
     <div class="content">
         @include('adminlte-templates::common.errors')
         <div class="box box-primary">

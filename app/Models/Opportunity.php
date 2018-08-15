b363ab31cdb5692beab001e5f7937686ec050de4
Nathan Rollins <nathan.rollins@asu.edu>
2018-08-15 13:59:10 -0700

    Remove unneeded FormAccessible trait

--- app/Models/Opportunity.php
+++ app/Models/Opportunity.php
@@ -3,7 +3,6 @@
 namespace SCCatalog\Models;
 
 use Carbon\Carbon;
-use Collective\Html\Eloquent\FormAccessible;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use RichanFongdasen\EloquentBlameable\BlameableTrait;
@@ -42,7 +41,6 @@ use Spatie\Sluggable\SlugOptions;
 class Opportunity extends Model
 {
     use BlameableTrait;
-    use FormAccessible;
     use HasSlug;
     use SoftDeletes;
 

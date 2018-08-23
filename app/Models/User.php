4f87f02aef2d11a310781f3896477287bfb8b01e
Nathan Rollins <nathan.rollins@asu.edu>
2018-08-22 18:14:39 -0700

    Uninstall Backpack Admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,15 @@
 
 namespace SCCatalog\Models;
 
-use Backpack\CRUD\CrudTrait;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Scout\Searchable;
 use RichanFongdasen\EloquentBlameable\BlameableTrait;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use BlameableTrait;
-    use CrudTrait;
-    use HasRoles;
     use Notifiable;
     use Searchable;
     use SoftDeletes;

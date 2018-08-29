2bc3c889d5bac888d443d4284dc74c248efa0023
Nathan Rollins <nathan.rollins@asu.edu>
2018-08-29 16:08:11 -0700

    Install new packages to enable boilerplate functions
    Temporary setup to see what pieces we want to keep

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,21 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.setPublicPath('public')
 
-mix.react('resources/assets/js/search.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/layout.scss', 'public/css');
+// mix.react('resources/assets/js/search.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css')
+//    .sass('resources/assets/sass/layout.scss', 'public/css');
+
+mix.sass('resources/assets/sass/frontend/app.scss', 'css/frontend.css')
+    .sass('resources/assets/sass/backend/app.scss', 'css/backend.css')
+    .js('resources/assets/js/frontend/app.js', 'js/frontend.js')
+    .js([
+        'resources/assets/js/backend/before.js',
+        'resources/assets/js/backend/app.js',
+        'resources/assets/js/backend/after.js'
+    ], 'js/backend.js');
+
+if (mix.inProduction() || process.env.npm_lifecycle_event !== 'hot') {
+    mix.version();
+}

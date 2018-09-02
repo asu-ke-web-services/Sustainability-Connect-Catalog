5eb3df8870dabdad16d5116a74eaba1a3006b68d
Nathan Rollins <nathan.rollins@asu.edu>
2018-09-02 13:07:29 -0700

    Update ide files

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-08-31 22:23:33.
+    * Generated on 2018-09-02 05:09:13.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper

b399e540a0e791522ed0ef6b7e8e9f23b1f77fc9
Nathan Rollins <nathan.rollins@asu.edu>
2018-09-19 14:47:32 -0700

    Add stats card on dashboard with data from db

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,6 +3,9 @@
 namespace SCCatalog\Http\Controllers\Backend;
 
 use SCCatalog\Http\Controllers\Controller;
+use SCCatalog\Repositories\Backend\Auth\UserRepository;
+use SCCatalog\Repositories\Backend\Opportunity\InternshipRepository;
+use SCCatalog\Repositories\Backend\Opportunity\ProjectRepository;
 
 /**
  * Class DashboardController.
@@ -10,10 +13,42 @@ use SCCatalog\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     /**
+     * @var ProjectRepository
+     */
+    private $projectRepository;
+
+    /**
+     * @var InternshipRepository
+     */
+    private $internshipRepository;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * DashboardController constructor.
+     *
+     * @param InternshipRepository $internshipRepository
+     * @param ProjectRepository    $projectRepository
+     * @param UserRepository       $userRepository
+     */
+    public function __construct(InternshipRepository $internshipRepository, ProjectRepository $projectRepository, UserRepository $userRepository)
+    {
+        $this->projectRepository = $projectRepository;
+        $this->internshipRepository = $internshipRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
      * @return \Illuminate\View\View
      */
     public function index()
     {
-        return view('backend.dashboard');
+        return view('backend.dashboard')
+            ->with('projectsTotal', $this->projectRepository->where('opportunityable_type', 'Project')->count())
+            ->with('internshipsTotal', $this->internshipRepository->where('opportunityable_type', 'Internship')->count())
+            ->with('activeUsersTotal', $this->userRepository->count());
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -16,7 +16,57 @@
 
         </div><!--col-->
     </div><!--row-->
-            <div class="card">
+
+    <div class="card-group mb-4">
+        <div class="card">
+            <div class="card-body">
+                <div class="h1 text-muted text-right mb-4">
+                    <i class="icon-notebook"></i>
+                </div>
+                <div class="text-value">{{ $projectsTotal }}</div>
+                <small class="text-muted text-uppercase font-weight-bold">Total Projects</small>
+                <div class="progress progress-xs mt-3 mb-0">
+                    <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <div class="h1 text-muted text-right mb-4">
+                    <i class="icon-notebook"></i>
+                </div>
+                <div class="text-value">{{ $internshipsTotal }}</div>
+                <small class="text-muted text-uppercase font-weight-bold">Total Internships</small>
+                <div class="progress progress-xs mt-3 mb-0">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <div class="h1 text-muted text-right mb-4">
+                    <i class="icon-user"></i>
+                </div>
+                <div class="text-value">{{ $activeUsersTotal }}</div>
+                <small class="text-muted text-uppercase font-weight-bold">Active Users</small>
+                <div class="progress progress-xs mt-3 mb-0">
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <div class="h1 text-muted text-right mb-4">
+                    <i class="icon-pie-chart"></i>
+                </div>
+                <div class="text-value">28%</div>
+                <small class="text-muted text-uppercase font-weight-bold">Returning Visitors</small>
+                <div class="progress progress-xs mt-3 mb-0">
+                    <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="row">
         <div class="col-lg-6">

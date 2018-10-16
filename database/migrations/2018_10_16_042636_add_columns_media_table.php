cde8e1507733f916c72b34fa9c941c79cefd0dac
Nathan Rollins <nathan.rollins@asu.edu>
2018-10-15 22:31:19 -0700

    Add db migration for additional columns on media table

---
+++ database/migrations/2018_10_16_042636_add_columns_media_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->text('comments')->nullable();
+            $table->softDeletes();
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->integer('deleted_by')->unsigned()->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['comments', 'deleted_at', 'created_by', 'updated_by', 'deleted_by']);
+        });
+    }
+}

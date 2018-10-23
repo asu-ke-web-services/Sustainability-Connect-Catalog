6e175ecfbba69885666bdde29c4766ed08b1863d
Nathan Rollins <nathan.rollins@asu.edu>
2018-10-23 15:39:55 -0700

    Update frontend routes

--- routes/frontend/opportunity.php
+++ routes/frontend/opportunity.php
@@ -15,10 +15,16 @@ Route::group([
     Route::get('project/submit', 'ProjectController@create')->name('project.create');
     Route::post('project', 'ProjectController@store')->name('project.store');
     Route::get('project/{project}', 'ProjectController@show')->name('project.show');
+    Route::post('project/follow/{project}', 'ProjectController@follow')->name('project.follow');
+    Route::post('project/unfollow/{project}', 'ProjectController@unfollow')->name('project.unfollow');
+    Route::post('project/apply/{project}', 'ProjectController@apply')->name('project.apply');
 
     /*
      * Internship CRUD
      */
     Route::get('internship', 'InternshipController@index')->name('internship.index');
     Route::get('internship/{internship}', 'InternshipController@show')->name('internship.show');
+    Route::post('internship/follow/{internship}', 'InternshipController@follow')->name('internship.follow');
+    Route::post('internship/unfollow/{internship}', 'InternshipController@unfollow')->name('internship.unfollow');
+    Route::post('internship/apply/{internship}', 'InternshipController@apply')->name('internship.apply');
 });

189178cdbbf56229601b0146fa9d6e6d2037040f
Nathan Rollins <nathan.rollins@asu.edu>
2018-10-28 09:41:52 -0700

    Remove tabbed content on project SearchApp view
    Not ready for that yet

--- resources/views/frontend/opportunity/project/search.blade.php
+++ resources/views/frontend/opportunity/project/search.blade.php
@@ -4,18 +4,9 @@
     <noscript>
     You need to enable JavaScript to run this app.
     </noscript>
-	<ul class="nav nav-tabs">
-	  <li role="presentation" class="active"><a href="#open">Open Listings</a></li>
-	  <li role="presentation"><a href="#past">Past Projects</a></li>
-	</ul>
-	<div class="tab-content clearfix">
-		<div class="tab-pane active" id="open">
-			<div id="search"></div>
-		</div>
-		<div class="tab-pane" id="past">
-			<div id="search"></div>
-		</div>
-	</div>
+    <div class="container-fluid">
+    	<div id="search"></div>
+    </div>
 @endsection
 
 @section('javascript')

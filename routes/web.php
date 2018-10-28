8ca71d5d03534c75429d206595b9ed37a41c2077
Nathan Rollins <nathan.rollins@asu.edu>
2018-10-27 21:44:28 -0700

    Clean up web routes file

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,6 @@ Route::group(['namespace' => 'Frontend', 'as' => 'frontend.'], function () {
     include_route_files(__DIR__.'/frontend/');
 });
 
-// Route::resource('projects', 'ProjectController');
-// Route::resource('internships', 'InternshipController');
-// Route::get('projects/submit_idea', 'ProjectController@create_idea');
-
-// Route::get('projects/{project}/admin', 'ProjectController@show_admin')->name('projects.show_admin');
-// Route::get('internships/{internship}/admin', 'InternshipController@show_admin')->name('internships.show_admin');
-
-// Route::get('projects/{project}/follow', 'OpportunityUserController@add_follower')->name('projects.add_follower');
-
-
 /*
  * Backend Routes
  * Namespaces indicate folder structure

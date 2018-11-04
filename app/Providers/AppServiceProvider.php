baef27d0b691fbc15e015de29b1cb970b6bb07ff
Nathan Rollins <nathan.rollins@asu.edu>
2018-11-04 13:39:43 -0700

    Disable shortened string lengths setting
    So long as we ensure our database servers are configured with large indices, no need to shorten our strings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -70,7 +70,7 @@ class AppServiceProvider extends ServiceProvider
 
         // Set the default string length for Laravel5.4
         // https://laravel-news.com/laravel-5-4-key-too-long-error
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);
 
         // Paginator::useBootstrapThree();
 

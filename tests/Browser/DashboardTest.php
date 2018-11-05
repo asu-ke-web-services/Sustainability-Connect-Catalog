89c97fff4572dfe5d23a39fd854fd28769d9251c
Nathan Rollins <nathan.rollins@asu.edu>
2018-11-05 08:29:11 -0700

    Wip

--- tests/Browser/DashboardTest.php
+++ tests/Browser/DashboardTest.php
@@ -6,7 +6,7 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ExampleTest extends DuskTestCase
+class DashboardTest extends DuskTestCase
 {
     /**
      * A basic browser test example.
@@ -16,8 +16,8 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+            $browser->visit('/dashboard')
+                    ->assertSee('Dashboard');
         });
     }
 }

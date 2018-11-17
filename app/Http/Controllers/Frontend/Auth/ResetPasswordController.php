73e4bf7203b6997bb1729dc6f5644000a4f2f8ba
Nathan Rollins <nathan.rollins@asu.edu>
2018-11-17 12:52:46 -0700

    Update Laravel to 5.7

--- app/Http/Controllers/Frontend/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Frontend/Auth/ResetPasswordController.php
@@ -108,10 +108,11 @@ class ResetPasswordController extends Controller
     /**
      * Get the response for a successful password reset.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @param  string  $response
      * @return \Illuminate\Http\RedirectResponse
      */
-    protected function sendResetResponse($response)
+    protected function sendResetResponse(Request $request, $response)
     {
         return redirect()->route(home_route())->withFlashSuccess(trans($response));
     }

ed17244f3f16dd8289ce66e14d37e411dca67002
Nathan Rollins <nathan.rollins@asu.edu>
2018-11-19 09:34:00 -0700

    Disable follow also

--- resources/views/frontend/opportunity/internship/sidebar.blade.php
+++ resources/views/frontend/opportunity/internship/sidebar.blade.php
@@ -1,26 +1,27 @@
 <div id="secondary" class="widget-area row" role="complementary">
     <div style="width: 285px;" id="sidebarNav" class="sidebar-nav affix-top">
     @if ($logged_in_user)
-        <aside id="internship-favorites" class="widget widget_internship_favorites">
-        @if ($isFollowed)
-            {{ html()->form('POST', route('frontend.internship.unfollow', $internship))->class('form-horizontal')->open() }}
-            {{ html()->button('Unfollow Internship', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
-            {{ html()->form()->close() }}
-        @else
-            {{ html()->form('POST', route('frontend.internship.follow', $internship))->class('form-horizontal')->open() }}
-            {{ html()->button('Follow Internship', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
-            {{ html()->form()->close() }}
-        @endif
-        </aside>
-{{--
-        <div>&nbsp;</div>
+            {{--
+            <aside id="internship-favorites" class="widget widget_internship_favorites">
+            @if ($isFollowed)
+                {{ html()->form('POST', route('frontend.internship.unfollow', $internship))->class('form-horizontal')->open() }}
+                {{ html()->button('Unfollow Internship', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
+                {{ html()->form()->close() }}
+            @else
+                {{ html()->form('POST', route('frontend.internship.follow', $internship))->class('form-horizontal')->open() }}
+                {{ html()->button('Follow Internship', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
+                {{ html()->form()->close() }}
+            @endif
+            </aside>
 
-        <aside id="internship-apply-now" class="widget widget_internship_apply">
-            {{ html()->form('POST', route('frontend.internship.apply', $internship))->class('form-horizontal')->open() }}
-            {{ html()->button('Request to Join', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
-            {{ html()->form()->close() }}
-        </aside>
---}}
+            <div>&nbsp;</div>
+
+            <aside id="internship-apply-now" class="widget widget_internship_apply">
+                {{ html()->form('POST', route('frontend.internship.apply', $internship))->class('form-horizontal')->open() }}
+                {{ html()->button('Request to Join', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
+                {{ html()->form()->close() }}
+            </aside>
+    --}}
     @endif
     </div>
 </div>
--- resources/views/frontend/opportunity/project/sidebar.blade.php
+++ resources/views/frontend/opportunity/project/sidebar.blade.php
@@ -1,26 +1,27 @@
 <div id="secondary" class="widget-area row" role="complementary">
     <div style="width: 285px;" id="sidebarNav" class="sidebar-nav affix-top">
     @if ($logged_in_user)
-        <aside id="project-favorites" class="widget widget_project_favorites">
-        @if ($logged_in_user && $isFollowed)
-            {{ html()->form('POST', route('frontend.project.unfollow', $project))->class('form-horizontal')->open() }}
-            {{ html()->button('Unfollow Project', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
-            {{ html()->form()->close() }}
-        @else
-            {{ html()->form('POST', route('frontend.project.follow', $project))->class('form-horizontal')->open() }}
-            {{ html()->button('Follow Project', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
-            {{ html()->form()->close() }}
-        @endif
-        </aside>
-{{--
-        <div>&nbsp;</div>
+            {{--
+            <aside id="project-favorites" class="widget widget_project_favorites">
+            @if ($logged_in_user && $isFollowed)
+                {{ html()->form('POST', route('frontend.project.unfollow', $project))->class('form-horizontal')->open() }}
+                {{ html()->button('Unfollow Project', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
+                {{ html()->form()->close() }}
+            @else
+                {{ html()->form('POST', route('frontend.project.follow', $project))->class('form-horizontal')->open() }}
+                {{ html()->button('Follow Project', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
+                {{ html()->form()->close() }}
+            @endif
+            </aside>
 
-        <aside id="project-apply-now" class="widget widget_project_apply">
-            {{ html()->form('POST', route('frontend.project.apply', $project))->class('form-horizontal')->open() }}
-            {{ html()->button('Request to Join', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
-            {{ html()->form()->close() }}
-        </aside>
---}}
+            <div>&nbsp;</div>
+
+            <aside id="project-apply-now" class="widget widget_project_apply">
+                {{ html()->form('POST', route('frontend.project.apply', $project))->class('form-horizontal')->open() }}
+                {{ html()->button('Request to Join', 'submit')->class(['btn', 'btn-primary', 'btn-sm']) }}
+                {{ html()->form()->close() }}
+            </aside>
+    --}}
     @endif
     </div>
 </div>

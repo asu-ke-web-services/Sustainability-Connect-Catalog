9b983dd399fb6695a9893ccff8dd25faf04bb063
Nathan Rollins <nathan.rollins@asu.edu>
2018-11-25 07:33:36 -0700

    Fix route parameters

--- routes/frontend/opportunity.php
+++ routes/frontend/opportunity.php
@@ -17,17 +17,17 @@ Route::group([
     Route::get('project/completed', 'ProjectController@completed')->name('project.completed');
     Route::get('project/submit', 'ProjectController@create')->name('project.create');
     Route::post('project', 'ProjectController@store')->name('project.store');
-    Route::get('project/{opportunity}', 'ProjectController@show')->name('project.show');
-    Route::post('project/follow/{opportunity}', 'ProjectFollowerController@follow')->name('project.follow');
-    Route::post('project/unfollow/{opportunity}', 'ProjectFollowerController@unfollow')->name('project.unfollow');
-    Route::post('project/apply/{opportunity}', 'ProjectController@apply')->name('project.apply');
+    Route::get('project/{project}', 'ProjectController@show')->name('project.show');
+    Route::post('project/follow/{project}', 'ProjectFollowerController@follow')->name('project.follow');
+    Route::post('project/unfollow/{project}', 'ProjectFollowerController@unfollow')->name('project.unfollow');
+    Route::post('project/apply/{project}', 'ProjectController@apply')->name('project.apply');
 
     /*
      * Internship CRUD
      */
     Route::get('internship', 'InternshipController@index')->name('internship.index');
-    Route::get('internship/{opportunity}', 'InternshipController@show')->name('internship.show');
-    Route::post('internship/follow/{opportunity}', 'InternshipFollowerController@follow')->name('internship.follow');
-    Route::post('internship/unfollow/{opportunity}', 'InternshipFollowerController@unfollow')->name('internship.unfollow');
-    Route::post('internship/apply/{opportunity}', 'InternshipController@apply')->name('internship.apply');
+    Route::get('internship/{internship}', 'InternshipController@show')->name('internship.show');
+    Route::post('internship/follow/{internship}', 'InternshipFollowerController@follow')->name('internship.follow');
+    Route::post('internship/unfollow/{internship}', 'InternshipFollowerController@unfollow')->name('internship.unfollow');
+    Route::post('internship/apply/{internship}', 'InternshipController@apply')->name('internship.apply');
 });

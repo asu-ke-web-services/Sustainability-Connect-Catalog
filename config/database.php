d045dc4853e68c80e53c42ece9209c50704c7933
Nathan Rollins <nathan.rollins@asu.edu>
2019-01-09 14:49:17 -0700

    Disable connection timeouts for Redis per predis maintainer

--- config/database.php
+++ config/database.php
@@ -134,6 +134,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 0),
+            'read_write_timeout' => -1,
         ],
 
     ],

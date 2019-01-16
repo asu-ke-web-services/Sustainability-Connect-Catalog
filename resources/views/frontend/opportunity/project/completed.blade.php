b2f531bb1dfe61fa2713e4310715c823c6218b2b
Nathan Rollins <nathan.rollins@asu.edu>
2019-01-16 13:31:58 -0700

    Add scripts to additional React-based views

--- resources/views/frontend/opportunity/project/completed.blade.php
+++ resources/views/frontend/opportunity/project/completed.blade.php
@@ -10,5 +10,7 @@
 @endsection
 
 @section('javascript')
+    <script src="{{mix('/js/manifest.js')}}" ></script>
+    <script src="{{mix('js/vendor.js')}}" ></script>
     <script src="{{mix('js/searchCompletedProject.js')}}" ></script>
 @endsection

291eb559c17e29e2f799bcc43af118f26069a917
Nathan Rollins <nathan.rollins@asu.edu>
2019-01-20 19:37:58 -0700

    Change route redirect from CLosure

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -10,9 +10,7 @@ use SCCatalog\Http\Controllers\Frontend\User\DashboardController;
  * Frontend Controllers
  * All route names are prefixed with 'frontend.'.
  */
-Route::get('/', function () {
-    return redirect()->route('frontend.user.dashboard');
-})->name('index');
+Route::redirect('/', '/dashboard', 301)->name('index');
 // Route::get('contact', [ContactController::class, 'index'])->name('contact');
 // Route::post('contact/send', [ContactController::class, 'send'])->name('contact.send');
 

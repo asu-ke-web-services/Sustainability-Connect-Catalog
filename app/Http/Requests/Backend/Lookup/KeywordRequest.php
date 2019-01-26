9ad506f9683b9f003c3e0a10e53b8d5d9bdc6ef9
Nathan Rollins <nathan.rollins@asu.edu>
2019-01-25 17:51:21 -0700

    Fix Keyword request validation rules

--- app/Http/Requests/Backend/Lookup/KeywordRequest.php
+++ app/Http/Requests/Backend/Lookup/KeywordRequest.php
@@ -28,8 +28,9 @@ class KeywordRequest extends FormRequest
     public function rules()
     {
         return [
-        	'order' => 'required',
+        	'order' => 'nullable|integer',
             'name'  => 'required|max:250',
+            'slug'  => 'nullable|string|max:255',
         ];
     }
 
@@ -41,9 +42,7 @@ class KeywordRequest extends FormRequest
     public function attributes()
     {
         return [
-            'order' => 'nullable|integer',
-            'name'  => ['required', 'string', 'max:250', Rule::unique('keywords')],
-            'slug'  => 'nullable|string|max:255',
+            //
         ];
     }
 

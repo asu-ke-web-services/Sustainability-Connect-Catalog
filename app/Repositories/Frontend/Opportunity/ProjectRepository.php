e1d4c17bcc6f090283ca791a0e9a725e87a1ad48
Nathan Rollins <nathan.rollins@asu.edu>
2019-02-27 11:32:06 -0700

    Update frontend Project Repository with create method from backend

--- app/Repositories/Frontend/Opportunity/ProjectRepository.php
+++ app/Repositories/Frontend/Opportunity/ProjectRepository.php
@@ -2,7 +2,13 @@
 
 namespace SCCatalog\Repositories\Frontend\Opportunity;
 
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\LengthAwarePaginator;
+use SCCatalog\Exceptions\GeneralException;
+use SCCatalog\Events\Backend\Opportunity\ProjectCreated;
+use SCCatalog\Models\Address\Address;
+use SCCatalog\Models\Lookup\Affiliation;
 use SCCatalog\Models\Opportunity\Project;
 use SCCatalog\Repositories\BaseRepository;
 
@@ -47,6 +53,85 @@ class ProjectRepository extends BaseRepository
             ->completed()
             ->orderBy($orderBy, $sort)
             ->paginate($paged);
+    }
+
+    /**
+     * Create a new Project record in the database.
+     *
+     * @param array $data
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     * @throws \Throwable
+     */
+    public function create(array $data)
+    {
+        if (null != $data['opportunity_start_at']) {
+            $data['opportunity_start_at'] = Carbon::parse($data['opportunity_start_at']);
+        }
+
+        if (null != $data['opportunity_end_at']) {
+            $data['opportunity_end_at'] = Carbon::parse($data['opportunity_end_at']);
+        }
+
+        // if (null != $data['listing_start_at']) {
+        //     $data['listing_start_at'] = Carbon::parse($data['listing_start_at']);
+        // }
+
+        // if (null != $data['listing_end_at']) {
+        //     $data['listing_end_at'] = Carbon::parse($data['listing_end_at']);
+        // }
+
+        // if (null != $data['application_deadline_at']) {
+        //     $data['application_deadline_at'] = Carbon::parse($data['application_deadline_at']);
+        // }
+
+        // If text deadline value is set, that overrides any value in the date field, which is to be set
+        // to far-future date for Algolia search purposes.
+        // if (null != $data['application_deadline_text']) {
+        //     $data['application_deadline_at'] = Carbon::create(2030, 12, 31, 23, 59);
+        // }
+
+
+        return DB::transaction(function () use ($data) {
+
+            $project = $this->model->create($data);
+
+            if ($project) {
+                // save Addresses
+                if ( isset($data['addresses'] ) ) {
+                    foreach ($data['addresses'] as $address) {
+                        $project->addresses()->save(Address::firstOrCreate($address));
+                    }
+                }
+
+                // attach Affiliations
+                if ( isset($data['affiliations'] ) ) {
+                    foreach ($data['affiliations'] as $affiliation) {
+                        $project->affiliations()->attach($affiliation);
+                    }
+                }
+
+                // attach Categories
+                if ( isset($data['categories'] ) ) {
+                    foreach ($data['categories'] as $category) {
+                        $project->categories()->attach($category);
+                    }
+                }
+
+                // attach Keywords
+                if ( isset($data['keywords'] ) ) {
+                    foreach ($data['keywords'] as $keyword) {
+                        $project->keywords()->attach($keyword);
+                    }
+                }
+
+                event(new ProjectCreated($project));
+
+                return $project;
+            }
+
+            throw new GeneralException(__('exceptions.backend.opportunity.create_error'));
+        });
     }
 
 }

867a47b4f8acb4119ff97a7a33686aec8b77962f
Nathan Rollins <nathan.rollins@asu.edu>
2019-03-04 09:46:37 -0700

    Code alignment

--- resources/views/frontend/opportunity/internship/search_active.blade.php
+++ resources/views/frontend/opportunity/internship/search_active.blade.php
@@ -3,58 +3,58 @@
 @section('content')
     <div class="container-fluid" style="min-height: 700px">
         <div class="box">
-        <div class="box-header">
-          <h3 class="box-title">Active Internships</h3>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body" style="font-size: .8em;">
-          <table id="datatable" class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Organization</th>
-                    <th>Availability</th>
-                    <th>City</th>
-                    <th>Apply By</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($internships as $internship)
-                    @php
-                        $accessAffiliations = $internship->affiliations
-                            ->filter(function ($affiliation) {
-                                return $affiliation->access_control;
-                            })
-                            ->map(function ($affiliation) {
-                                return $affiliation->slug;
-                            })->toArray();
-
-                        $restrictAccess = false;
-                        foreach ($accessAffiliations as $restriction) {
-                            if (!in_array($restriction, $userAccessAffiliations )) {
-                                $restrictAccess = true;
-                            }
-                        }
-                    @endphp
+            <div class="box-header">
+                <h3 class="box-title">Active Internships</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body" style="font-size: .8em;">
+                <table id="datatable" class="table table-bordered table-striped">
+                    <thead>
                     <tr>
-                        <td>
-                        @if (!$canViewRestricted && $restrictAccess)
-                            View Restricted for SOS majors only
-                        @else
-                            <b><a href="{!! url()->previous() !!}">{{ ucwords($internship->name) }}</a></b>
-                        @endif
-                        </td>
-                        <td>
-                        @if (!$canViewRestricted && $restrictAccess)
-                            View Restricted for SOS majors only
-                        @else
-                            {{ ucwords($internship->organization->name) }}
-                        @endif
-                        </td>
-                        <td class="icon-column">
-                            @foreach ($internship->affiliations as $icon)
-                                @unless(empty($icon->frontend_fa_icon))
-                                <span class="fa-stack" data-toggle="tooltip" data-container="body" title="{{ $icon->help_text }}">
+                        <th>Name</th>
+                        <th>Organization</th>
+                        <th>Availability</th>
+                        <th>City</th>
+                        <th>Apply By</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($internships as $internship)
+                        @php
+                            $accessAffiliations = $internship->affiliations
+                                ->filter(function ($affiliation) {
+                                    return $affiliation->access_control;
+                                })
+                                ->map(function ($affiliation) {
+                                    return $affiliation->slug;
+                                })->toArray();
+
+                            $restrictAccess = false;
+                            foreach ($accessAffiliations as $restriction) {
+                                if (!in_array($restriction, $userAccessAffiliations )) {
+                                    $restrictAccess = true;
+                                }
+                            }
+                        @endphp
+                        <tr>
+                            <td>
+                                @if (!$canViewRestricted && $restrictAccess)
+                                    View Restricted for SOS majors only
+                                @else
+                                    <b><a href="{!! url()->previous() !!}">{{ ucwords($internship->name) }}</a></b>
+                                @endif
+                            </td>
+                            <td>
+                                @if (!$canViewRestricted && $restrictAccess)
+                                    View Restricted for SOS majors only
+                                @else
+                                    {{ ucwords($internship->organization->name) }}
+                                @endif
+                            </td>
+                            <td class="icon-column">
+                                @foreach ($internship->affiliations as $icon)
+                                    @unless(empty($icon->frontend_fa_icon))
+                                        <span class="fa-stack" data-toggle="tooltip" data-container="body" title="{{ $icon->help_text }}">
                                     @php
                                         $icon = json_decode($icon->frontend_fa_icon);
                                     @endphp
@@ -62,31 +62,31 @@
                                     <div><div class="{{ $icon[1]->className ?? null }}">{{ $icon[1]->content ?? null }}</div>
                                     </div>
                                 </span>
-                                @endunless
-                            @endforeach
-                        </td>
-                        <td>
-                            @if ($internship->addresses->count())
-                                @foreach ($internship->addresses as $address)
-                                    {{ ucwords($address->city) }}
+                                    @endunless
                                 @endforeach
-                            @else
-                                {{ __('labels.general.none') }}
-                            @endif
-                        </td>
-                        <td>{{
-                             null != $internship->application_deadline_text
-                                ? $internship->application_deadline_text
-                                : (null !== $internship->application_deadline_at ? $internship->application_deadline_at->toDateString() : null)
-                        }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-          </table>
+                            </td>
+                            <td>
+                                @if ($internship->addresses->count())
+                                    @foreach ($internship->addresses as $address)
+                                        {{ ucwords($address->city) }}
+                                    @endforeach
+                                @else
+                                    {{ __('labels.general.none') }}
+                                @endif
+                            </td>
+                            <td>{{
+                                !empty($internship->application_deadline_text)
+                                    ? $internship->application_deadline_text
+                                    : (!empty($internship->application_deadline_at) ? $internship->application_deadline_at->toDateString() : null)
+                            }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.box-body -->
         </div>
-        <!-- /.box-body -->
-      </div>
-      <!-- /.box -->
+        <!-- /.box -->
     </div>
 @endsection
 
@@ -100,58 +100,58 @@
 
         .icon-column .fa,
         .icon-legend .fa {
-          margin-right: .5rem;
+            margin-right: .5rem;
         }
 
         .fa-maroon {
-          color: #8c1d40;
+            color: #8c1d40;
         }
 
         .fa-brick-red {
-          color: #d23153;
+            color: #d23153;
         }
 
         .fa-blue {
-          color: #00a3e0;
+            color: #00a3e0;
         }
 
         .fa-blue-darkened {
-          color: #010598;
+            color: #010598;
         }
 
         .fa-green {
-          color: #78be20;
+            color: #78be20;
         }
 
         .fa-green-darkened {
-          color: #168026;
+            color: #168026;
         }
 
         .fa-orange {
-          color: #ff7f32;
+            color: #ff7f32;
         }
 
         .fa-gold {
-          color: #ffc627;
+            color: #ffc627;
         }
 
         .fa-gold-darkened {
-          color: #c69304;
+            color: #c69304;
         }
 
         .fa-abbey {
-          color: #4f5557;
+            color: #4f5557;
         }
 
         .text-muted {
-          margin-top: 9px;
-          margin-left: 7px;
+            margin-top: 9px;
+            margin-left: 7px;
         }
 
         @media (max-width: 768px) {
-          .input-group {
-            margin-top: 30px;
-          }
+            .input-group {
+                margin-top: 30px;
+            }
         }
     </style>
 @endsection

4d4fa9561baa1fef8ef298f40ffdcb4b80692735
Nathan Rollins <nathan.rollins@asu.edu>
2019-03-29 16:28:45 -0700

    Update frontend dashboard sidebar menus

--- resources/views/frontend/includes/coreui/sidebar.blade.php
+++ resources/views/frontend/includes/coreui/sidebar.blade.php
@@ -16,51 +16,27 @@
                 Tasks
             </li>
 
-            <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/opportunity*'), 'open') }}">
-                <a class="nav-link nav-dropdown-toggle {{ active_class(Active::checkUriPattern('admin/opportunity*')) }}" href="#">
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link nav-dropdown-toggle" href="#">
                     <i class="icon-notebook"></i> {{ __('Projects') }}
                 </a>
 
                 <ul class="nav-dropdown-items">
 
                     <li class="nav-item">
-                        <a class="nav-link {{ active_class(Active::checkUriPattern('admin/opportunity/project*')) }}" href="#">
+                        <a class="nav-link" href="{{ route('frontend.opportunity.project.submission.create') }}">
                             Submit Project Proposal
                         </a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link {{ active_class(Active::checkUriPattern('admin/opportunity/internship*')) }}" href="#">
+                        <a class="nav-link" href="{{ route('frontend.opportunity.project.create') }}">
                             Create Full Project Listing
                         </a>
                     </li>
 
                 </ul>
             </li>
-
-            <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/opportunity*'), 'open') }}">
-                <a class="nav-link nav-dropdown-toggle {{ active_class(Active::checkUriPattern('admin/opportunity*')) }}" href="#">
-                    <i class="icon-notebook"></i> {{ __('Internships') }}
-                </a>
-
-                <ul class="nav-dropdown-items">
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(Active::checkUriPattern('admin/opportunity/project*')) }}" href="#">
-                            Submit Internship Proposal
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(Active::checkUriPattern('admin/opportunity/internship*')) }}" href="#">
-                            Create Full Internship Listing
-                        </a>
-                    </li>
-
-                </ul>
-            </li>
-
-
         </ul>
     </nav>
 </div><!--sidebar-->

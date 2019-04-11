3a8d8e619856848beb2e982e28b1946a53ff9a32
Nathan Rollins <nathan.rollins@asu.edu>
2019-04-11 12:35:02 -0700

    Add missing internship request classes

---
+++ app/Http/Requests/Frontend/Opportunity/EditFullInternshipRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SCCatalog\Http\Requests\Frontend\Opportunity;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class EditFullInternshipRequest.
+ */
+class EditFullInternshipRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}

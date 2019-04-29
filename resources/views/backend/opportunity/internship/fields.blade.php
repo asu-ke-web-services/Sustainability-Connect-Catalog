281d9a1084ca32755c2785debb92b360ed11acdd
Nathan Rollins <nathan.rollins@asu.edu>
2019-04-29 13:05:51 -0700

    Fix syntax for loading form javascript in internship form pages
    We weren't loading CKEditor on Internship forms because the wrong (old) syntax was being used for pushing the script into the page layout.

--- resources/views/backend/opportunity/internship/fields.blade.php
+++ resources/views/backend/opportunity/internship/fields.blade.php
@@ -427,7 +427,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css" />
 @endpush
 
-@section('javascript')
+@push('scripts')
     <script src="https://cdn.ckeditor.com/4.11.3/standard/ckeditor.js"></script>
     <script>
         CKEDITOR.replace( 'description' );
@@ -476,4 +476,4 @@
             }
         });
     </script>
-@endsection
+@endpush

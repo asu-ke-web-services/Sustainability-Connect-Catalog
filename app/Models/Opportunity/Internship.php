ed900d4c82c0c833ea52a228b4c1966098609645
Nathan Rollins <nathan.rollins@asu.edu>
2019-05-08 09:33:13 -0700

    Fix app deadline tests in Internship visibility

--- app/Models/Opportunity/Internship.php
+++ app/Models/Opportunity/Internship.php
@@ -255,7 +255,7 @@ class Internship extends Model implements HasMedia
     {
         return (9 === $this->opportunity_status_id &&
             null !== $this->application_deadline_at &&
-            $this->application_deadline_at->greaterThan(Carbon::today())
+            $this->application_deadline_at->greaterThan(Carbon::yesterday())
 
             // $this->listing_start_at !== null &&
             // $this->listing_start_at->lessThan(Carbon::today()) &&
@@ -538,7 +538,7 @@ class Internship extends Model implements HasMedia
         return $query
             ->where([
                 ['application_deadline_at', '<>', null],
-                ['application_deadline_at', '>', Carbon::today()],
+                ['application_deadline_at', '>', Carbon::yesterday()],
             ])
             ->where('opportunity_status_id', 9);
     }

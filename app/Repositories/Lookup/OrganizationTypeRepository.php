5ab52a90fe130ccdb5a2b7a98a2105e70e75464e
Nathan Rollins <nathan.rollins@asu.edu>
2019-05-15 16:42:24 -0700

    Add default sorts to all Lookup repositories
    Specifically to make Keywords sort by name rather than the `order` column

--- app/Repositories/Lookup/OrganizationTypeRepository.php
+++ app/Repositories/Lookup/OrganizationTypeRepository.php
@@ -11,6 +11,15 @@ use SCCatalog\Repositories\BaseRepository;
 class OrganizationTypeRepository extends BaseRepository
 {
     /**
+     * Array of one or more ORDER BY column/value pairs.
+     *
+     * @var array
+     */
+    protected $orderBys = [
+        ['column' => 'order', 'direction' => 'asc'],
+    ];
+
+    /**
      * Configure the Model
      **/
     public function model()

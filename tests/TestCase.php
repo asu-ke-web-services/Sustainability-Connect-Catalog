d82af6dcb87c785132bb457a4c122d157fa1a60e
Nathan Rollins <nathan.rollins@asu.edu>
2019-10-11 12:51:52 -0700

    Apply sqlite fix for PHPUnit tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,7 +12,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
  */
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, SqliteForeignHotfix;
+
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->hotfixSqlite();
+    }
 
     /**
      * Create the admin role or return it if it already exists.

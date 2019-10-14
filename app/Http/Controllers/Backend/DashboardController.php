c17e60c70221f022fb2865a2b123ea0368840b29
Nathan Rollins <nathan.rollins@asu.edu>
2019-10-13 20:34:20 -0700

    Update backend dashboard template

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,6 +3,10 @@
 namespace SCCatalog\Http\Controllers\Backend;
 
 use SCCatalog\Http\Controllers\Controller;
+use SCCatalog\Models\Opportunity\Project;
+use SCCatalog\Repositories\Auth\Backend\UserRepository;
+use SCCatalog\Repositories\Opportunity\InternshipRepository;
+use SCCatalog\Repositories\Opportunity\ProjectRepository;
 
 /**
  * Class DashboardController.
@@ -10,10 +14,52 @@ use SCCatalog\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     /**
+     * @var Project
+     */
+    private $project;
+
+    /**
+     * @var ProjectRepository
+     */
+    private $projectRepository;
+
+    /**
+     * @var InternshipRepository
+     */
+    private $internshipRepository;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * DashboardController constructor.
+     *
+     * @param Project              $project
+     * @param InternshipRepository $internshipRepository
+     * @param ProjectRepository    $projectRepository
+     * @param UserRepository       $userRepository
+     */
+    public function __construct(Project $project, InternshipRepository $internshipRepository, ProjectRepository $projectRepository, UserRepository $userRepository)
+    {
+        $this->project = $project;
+        $this->projectRepository = $projectRepository;
+        $this->internshipRepository = $internshipRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
      * @return \Illuminate\View\View
      */
     public function index()
     {
-        return view('backend.dashboard');
+        return view('backend.dashboard')
+            ->with('activeProjectsCount', $this->projectRepository->getActiveCount())
+            ->with('completedProjectsCount', $this->projectRepository->getCompletedCount())
+            ->with('activeInternshipsCount', $this->internshipRepository->getActiveCount())
+            ->with('activeUsersCount', $this->userRepository->getActiveCount())
+            ->with('projectsUnderReview', $this->projectRepository->getProposalReviewsPaginated(100, 'created_at', 'asc'))
+            ->with('newUsersToReview', $this->userRepository->getNeedsAffiliationReviewPaginated(100, '', 'created_at', 'asc'));
     }
 }
